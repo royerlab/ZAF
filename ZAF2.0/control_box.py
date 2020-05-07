4f2f41b4adc04e5a94343a109e45e54fe91ac76d
AhmetCanSolak <asolak14@ku.edu.tr>
2020-05-07 16:31:05 -0700

    zaf2.0 implemented tested

---
+++ ZAF2.0/control_box.py
@@ -0,0 +1,55 @@
+from time import sleep
+
+import serial
+
+
+class ControlBox:
+
+    def __init__(self, port="/dev/ttyACM0", rate=500000,  nb_arduino=2):
+        self.port = port
+        self.rate = rate
+        self.nb_arduino = nb_arduino
+
+        self.conn = serial.Serial(self.port, baudrate=self.rate, timeout=2.0)
+        self.conn.flushInput()
+
+    get_state = str.encode("#?\n")
+    shutdown_all = str.encode("#!\n")
+    debug = lambda x: str.encode("#d" + str(int(x)) + "\n'")
+
+    test_valves = str.encode("#tv 3\n")
+
+    def initialize(self):
+        self.conn.write(self.get_state)
+        rcv = self.conn.read(43)
+        print(rcv)
+
+        self.conn.write(self.get_state)
+        rcv = self.conn.read(78)
+        print(rcv)
+        rcv = self.conn.read(78)
+        print(rcv)
+
+    def open_valve(self, index):
+        open_valve_command = str.encode("#vo" + " " + str(index) + " \n")
+
+        self.conn.write(open_valve_command)
+        rcv = self.conn.read(6)
+        print(rcv)
+        sleep(1)
+
+    def close_valve(self, index):
+        close_valve_command = str.encode("#vc" + " " + str(index) + " \n")
+
+        self.conn.write(close_valve_command)
+        rcv = self.conn.read(6)
+        print(rcv)
+        sleep(1)
+
+    def set_pwm(self, index, value):
+        set_pwm_command = str.encode("#p " + str(index) + " " + str(value) + "\n")
+
+        self.conn.write(set_pwm_command)
+        rcv = self.conn.read(6)
+        print(rcv)
+        sleep(1)
---
+++ ZAF2.0/fishfeed.py
@@ -0,0 +1,145 @@
+import datetime
+from time import sleep
+from RPi import GPIO
+
+from context import Context
+
+
+def initialize():
+    Context.initialize()
+
+    Context.DAY, Context.TIME = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S').split()
+
+    # Initialize water sensor
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(Context.water_sensor, GPIO.OUT)
+    GPIO.output(Context.water_sensor, GPIO.LOW)
+    sleep(0.05)
+    GPIO.setup(Context.water_sensor, GPIO.IN)
+
+
+def priming():
+    # prime water In
+    for _ in range(3):
+        Context.check_water_sensor()
+        Context.run_pump([Context.water_in], duration=0.5)
+
+    # prime water Out
+    for _ in range(1):
+        Context.check_water_sensor()
+        Context.run_pump(pumps=[Context.water_out1, Context.water_out2], duration=0.5)
+
+
+def prepare():
+    # pour food into fishfeeder
+    Context.rotate_food_servo(180)
+    Context.rotate_food_servo(0)
+
+    # bring clean water to fishfeeder
+    for _ in range(1):
+        Context.check_water_sensor()
+        Context.run_pump(pumps=[Context.water_in], duration=0.5)
+
+    Context.STATUS = "FoodPrepared"
+
+
+def stream():
+    # stream water to fish tanks
+    for i in range(1):
+        for _ in range(1):
+            Context.check_water_sensor()
+            Context.run_pump(pumps=[Context.water_out1, Context.water_out2], duration=0.5)
+
+    # bring water and stream fishfeeder
+    for _ in range(1):
+        for _ in range(1):
+            Context.check_water_sensor()
+            Context.run_pump(pumps=[Context.water_in], duration=0.5)
+
+        for _ in range(1):
+            Context.check_water_sensor()
+            Context.run_pump(pumps=[Context.water_out1, Context.water_out2], duration=0.5)
+
+
+def clean():
+    for _ in range(1):
+        # bring clean water to fishfeeder
+        Context.check_water_sensor()
+        Context.run_pump(pumps=[Context.water_in], duration=0.5)
+
+    for _ in range(1):
+        # thrash water from fishfeeder
+        Context.check_water_sensor()
+        Context.run_pump(pumps=[Context.water_out1, Context.water_out2], duration=0.5)
+
+    for _ in range(1):
+        for _ in range(1):
+            # bring clean water to fishfeeder
+            Context.check_water_sensor()
+            Context.run_pump(pumps=[Context.water_in], duration=0.5)
+
+        for _ in range(1):
+            # thrash water from fishfeeder
+            Context.check_water_sensor()
+            Context.run_pump(pumps=[Context.water_out1, Context.water_out2], duration=0.5)
+
+    for _ in range(1):
+        for _ in range(1):
+            # bring clean water to fishfeeder
+            Context.check_water_sensor()
+            Context.run_pump(pumps=[Context.water_in], duration=0.5)
+
+        for _ in range(1):
+            # thrash water from fishfeeder
+            Context.check_water_sensor()
+            Context.run_pump(pumps=[Context.water_out1, Context.water_out2], duration=0.5)
+
+    Context.STATUS = "Cleaned"
+
+
+def finalize():
+    Context.control_box.set_pwm(Context.safety_pump, 0)
+    print(Context.DAY, Context.TIME, Context.STATUS)
+
+
+def run():
+    try:
+        # initialize ports
+        initialize()
+        print("initialized")
+
+        # Run feeding sequence for each tank
+        for valve_index in range(1):
+            # priming pumps
+            priming()
+            print("pumps primed")
+
+            # prepare food
+            prepare()
+            print("food prepared")
+
+            # open the right valve
+            Context.control_box.open_valve(valve_index)
+
+            # deliver food to containers
+            stream()
+            print("food water mix streamed")
+
+            # clean the tank
+            clean()
+            print("tanks cleaned")
+
+            # close the opened valve
+            Context.control_box.close_valve(valve_index)
+
+    except KeyboardInterrupt:
+        print("\nCtrl-C pressed.  Program exiting...")
+    finally:
+        # finalize
+        finalize()
+        print("finalized")
+
+
+if __name__ == '__main__':
+    run()
