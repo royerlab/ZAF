8156d928b24d494815700a2c5f721a1ad6401c9a
AhmetCanSolak <asolak14@ku.edu.tr>
2020-12-14 11:48:53 -0800

    initial commit, fix on window size, creation of gui entry point

---
+++ python/gui/gui.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from python.gui.windows.mainwindow import MainWindow
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.processEvents()
+    ctrl = MainWindow()
+    sys.exit(app.exec_())
--- python/gui/windows/mainwindow.py
+++ python/gui/windows/mainwindow.py
@@ -9,8 +9,8 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('ZAF 2.0')
-        self.setGeometry(0, 0, 800, 480)
-        self.setFixedSize(800, 480)
+        self.setGeometry(0, 0, 640, 455)
+        self.setFixedSize(640, 455)
 
         self.statusBar = QStatusBar()
         self.setStatusBar(self.statusBar)
